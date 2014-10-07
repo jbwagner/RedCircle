601ae03fb286ecd2b0778bdd54ccb4e1a5b62d6a
lmstreng <laurel@laurelstreng.com>
2014-10-07 16:04:41 -0400

    Incorrect closing tag
    Updated </h2> to </h1>

--- page.php
+++ page.php
@@ -8,7 +8,7 @@
             
           <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
             <article>
-              <h1 class="page-title"><?php get_field('h1_title') ? the_field('h1_title') : the_title();?> </h2>
+              <h1 class="page-title"><?php get_field('h1_title') ? the_field('h1_title') : the_title();?> </h1>
               <?php the_content(); ?>
             </article>
           <?php endwhile; // end loop ?>
