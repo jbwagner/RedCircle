daf4ba2e94d85d665ef02590291d03bd328f813d
lmstreng <laurel@laurelstreng.com>
2014-12-09 15:20:14 -0500

    Hide empty page-title field

--- page.php
+++ page.php
@@ -8,7 +8,12 @@
             
           <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
             <article>
-              <h1 class="page-title"><?php get_field('h1_title') ? the_field('h1_title') : the_title();?> </h2>
+              <?php if(get_field('h1_title')): ?>
+                <h1 class="page-title"><?php the_field('h1_title'); ?></h1>
+              <?php else: ?>
+                <h1 class="page-title"><?php the_title(); ?></h1>
+              <?php endif; ?>
+
               <?php the_content(); ?>
             </article>
           <?php endwhile; // end loop ?>
