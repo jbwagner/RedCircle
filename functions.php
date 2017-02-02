8ec39f745754d702bdad4aeba59ad48399e4a99b
laurelstreng <laurel@laurelstreng.com>
2017-02-02 10:22:13 -0500

    Removed closing php

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@ if (function_exists('register_sidebar')) {
 }
 
 
-// Register nav menus         
+// Register nav menus
 if ( function_exists( 'register_nav_menus' ) ) {
   register_nav_menus(
     array(
@@ -46,23 +46,23 @@ if ( function_exists( 'register_nav_menus' ) ) {
 
 // Custom Login Screen
 // Change logo style
-function custom_login() { 
-  echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/css/custom-login.css" />'; 
+function custom_login() {
+  echo '<link rel="stylesheet" type="text/css" href="'.get_bloginfo('template_directory').'/css/custom-login.css" />';
 }
 add_action('login_head', 'custom_login');
 // Change logo url
-function change_wp_login_url() { 
-  return get_site_url(); 
-} 
+function change_wp_login_url() {
+  return get_site_url();
+}
 add_filter('login_headerurl', 'change_wp_login_url');
 // Change logo title
-function change_wp_login_title() { 
-  return 'Powered by ' . get_option('blogname'); 
-} 
+function change_wp_login_title() {
+  return 'Powered by ' . get_option('blogname');
+}
 add_filter('login_headertitle', 'change_wp_login_title');
 
 // Add support for Featured Images
-if ( function_exists( 'add_theme_support' ) ) { 
+if ( function_exists( 'add_theme_support' ) ) {
   add_theme_support( 'post-thumbnails' );
   set_post_thumbnail_size( 250, 250, true ); // default Post Thumbnail dimensions (cropped)
 }
@@ -76,20 +76,20 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 // Enqueue Styles & Scripts
 if(!is_admin()) {
   function load_styles_and_scripts() {
-    
+
     wp_register_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.8.3.min.js', false );
     wp_register_script( 'owl', get_template_directory_uri() . '/js/vendor/owl-carousel-2/owl.carousel.min.js', array('jquery'), '1.0', false );
     wp_register_script( 'main', get_template_directory_uri() . '/js/main.min.js', array('jquery'), '1.0', false );
-      
+
     wp_enqueue_script( 'modernizr' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'owl' );
     wp_enqueue_script( 'main' );
-    
+
     wp_enqueue_style('owlcss', get_template_directory_uri() . '/js/vendor/owl-carousel-2/assets/owl.carousel.css', false );
     wp_enqueue_style('gfont', 'http://fonts.googleapis.com/css?family=Raleway:400,600,700,900', false );
     wp_enqueue_style('mainstyle', get_template_directory_uri() . '/style.css', false, '1.0', 'screen');
-  
+
   }
   add_action( 'wp_enqueue_scripts', 'load_styles_and_scripts' );
 }
@@ -122,5 +122,3 @@ function address_shortcode() {
   return ob_get_clean();
 }
 add_shortcode('address', 'address_shortcode');
-
-?>
