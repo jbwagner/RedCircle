5ba45ae7a478b3e3509280b39371ef6f270798b8
laurelstreng <laurel@laurelstreng.com>
2017-02-02 10:33:07 -0500

    Added copyright to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
 
         <p class="copyright">&copy; <?php echo date('Y'); ?> <?php echo bloginfo('name'); ?>, All Rights Reserved. <a href="http://www.cocg.co/">Digital Marketing</a> and <a href="http://www.cocg.co/">Design</a> by <a href="http://www.cocg.co/">COCG./ A Division of Healthgrades</a></p>
-        
+
         <?php wp_footer(); ?>
 
     </body>
